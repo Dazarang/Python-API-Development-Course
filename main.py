3e54b9b5eaae9856f8e7ae50245409f7e4a3d79f
Dena Azarang <dena.azarang@hotmail.com>
2022-07-14 13:59:44 +0200

    Added validation class and some modification in existing functions

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
+from typing import Optional
 from fastapi import Body, FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
+
+# Class using pydantic to validate our request body
+class Post(BaseModel):
+    title: str
+    content: str
+    published: bool = True
+    rating: Optional[int] = None
+
+my_posts = [{"title": "title of post 1", "content": "content of post 1", "id": 1}, {"title": "favorite foods", "content": "I like pizza", "id": 2}]
 
 # A path operation:  
 # Decorator, when its applied to a function, it adds a path to the function, 
@@ -12,9 +23,16 @@ def root(): # The function is called root, async in the beginningoptional i.e yo
 
 @app.get("/posts")
 def get_posts():
-    return {"data": "This is your posts"}
+    return {"data": my_posts}
 
-@app.post("/createposts")
-def create_posts(payLoad: dict = Body(...)): # Extracts fields from body and convert to dictionary and store in payLoad
-    print(payLoad)
-    return {"new_post": f"title {payLoad['title']} content {payLoad['content']}"}
+# Extract data and send it back
+@app.post("/posts")
+#def create_posts(payLoad: dict = Body(...)): # Extracts fields from body and convert to dictionary and store in payLoad
+def create_posts(post: Post): # Automaticly extracts the data via post
+    print(post)
+    print(post.dict()) # Converts to dictionary
+    
+    my_posts.append(post.dict())
+    return {"data": post}
+
+# Data we want: title str, content str ##, category, Bool published. We use pydantic to validate the data
