891f8eb2c604e4d39919656342057cae40c4b71d
Dena Azarang <dena.azarang@hotmail.com>
2022-07-14 14:31:18 +0200

    Added function to retrieve post id

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from random import randrange
 from typing import Optional
 from fastapi import Body, FastAPI
 from pydantic import BaseModel
@@ -12,6 +13,11 @@ class Post(BaseModel):
     rating: Optional[int] = None
 
 my_posts = [{"title": "title of post 1", "content": "content of post 1", "id": 1}, {"title": "favorite foods", "content": "I like pizza", "id": 2}]
+
+def find_post(id): 
+    for p in my_posts:
+        if p["id"] == id:
+            return p
 
 # A path operation:  
 # Decorator, when its applied to a function, it adds a path to the function, 
@@ -28,7 +34,16 @@ def get_posts():
 # Extract data and send it back
 @app.post("/posts")
 def create_posts(post: Post): # Automaticly extracts the data via post
+    post_dict = post.dict() # Converts the post to a dictionary
+    post_dict["id"] = randrange(0, 1000000) # Adds an id to the post
+    my_posts.append(post_dict) # Adds the post to the list
     
-    my_posts.append(post.dict())
-    return {"data": post}
+    return {"data": post_dict} # Returns the post
 
+@app.get("/posts/{id}") # Extracts the id from the url, the id is a path parameter
+def get_post(id: int): #Fast api will auto extract that id and we can pass it directly to function
+    
+    post = find_post(int(id)) # Finds the post with the id, manually make it an int
+    print(post)
+    return {"post_detail": post}
+ 
