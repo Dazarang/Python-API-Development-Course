b51bf0452ff74866ebbf8f9883acc1e5d9d3419d
Dena Azarang <dena.azarang@hotmail.com>
2022-07-14 22:11:21 +0200

    Added SQLALCHEMY database py file

---
+++ app/database.py
@@ -0,0 +1,19 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+# SQLALCHEMY_DATABASE_URL = 'postgreeql://<username>:<password>@<ip-adress/hostname>/<database-name>'
+SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:password123@localhost/fastapi'
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL, echo=True) # echo is used to print the query
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine) # SessionLocal is a class that is used to create a session
+
+Base = declarative_base()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
