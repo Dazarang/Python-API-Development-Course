e88796321115505f4af0953d097098684c82bcfc
Dena Azarang <dena.azarang@hotmail.com>
2022-07-14 23:31:27 +0200

    Added response model

--- app/schemas.py
+++ app/schemas.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from pydantic import BaseModel
 
 class PostBase(BaseModel):
@@ -7,3 +8,11 @@ class PostBase(BaseModel):
 
 class PostCreate(PostBase):
     pass
+
+
+class Post(PostBase): # Response model
+    id: int
+    created_at: datetime
+    
+    class Config: # This is a class that is used to configure the Post class to be able to be used in the FastAPI framework
+        orm_mode = True 
