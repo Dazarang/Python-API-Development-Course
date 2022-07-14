8b8f940de6c33c28e72e56c08e7a8aef39d20783
Dena Azarang <dena.azarang@hotmail.com>
2022-07-15 00:08:09 +0200

    Added user validation and response model

--- app/schemas.py
+++ app/schemas.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr
 
 class PostBase(BaseModel):
     title: str
@@ -16,3 +16,17 @@ class Post(PostBase): # Response model
     
     class Config: # This is a class that is used to configure the Post class to be able to be used in the FastAPI framework
         orm_mode = True 
+        
+        
+        
+class UserCreate(BaseModel): # Response model
+    email: EmailStr
+    password: str
+    
+class UserOut(BaseModel):
+    id: int
+    email: EmailStr
+    created_at: datetime
+        
+    class Config:
+        orm_mode = True
