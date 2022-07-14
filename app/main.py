1d3d01ba5bfaa13fb6fe32eac28617cb960e8cdf
Dena Azarang <dena.azarang@hotmail.com>
2022-07-14 22:27:35 +0200

    Cleaned code and comments

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from random import randrange
+from turtle import pos
 from typing import Optional
 from fastapi import Body, FastAPI, Response, status, HTTPException, Depends
 from pydantic import BaseModel
@@ -15,8 +16,6 @@ models.Base.metadata.create_all(bind=engine)
 app = FastAPI()
 
 
-
-
 # Class using pydantic to validate our request body
 class Post(BaseModel):
     title: str
@@ -28,8 +27,8 @@ class Post(BaseModel):
 while True:
     
     try:
-        conn = psycopg2.connect(host = "localhost", database = "fastapi", user = "postgres", password = "password123", cursor_factory=RealDictCursor) # Connect to database
-        # cursor_factory is used to return columns as dictionaries
+        conn = psycopg2.connect(host = "localhost", database = "fastapi", user = "postgres", 
+                                password = "password123", cursor_factory=RealDictCursor) # Connect to database
         cursor = conn.cursor()
         print("Database connection was successful")
         break
@@ -38,7 +37,9 @@ while True:
         print("Error: ", error)
         time.sleep(3)
 
+
 my_posts = [{"title": "title of post 1", "content": "content of post 1", "id": 1}, {"title": "favorite foods", "content": "I like pizza", "id": 2}]
+
 
 def find_post(id): 
     for p in my_posts:
@@ -50,17 +51,19 @@ def find_index_post(id):
         if p["id"] == id:
             return i
 
-# A path operation:  
-# Decorator, when its applied to a function, it adds a path to the function, 
-#@ clearifies that it is a decorator then reference fastapi reference which is app and send get request
-# / is the root path e.g google.com/ is the same as google.com. It could be "/login" meaining url is google.com/login
+
 @app.get("/")
-def root(): # The function is called root, async in the beginningoptional i.e you want to talk to the server asyncronously
+def root():
     return {"message": "Hello World"}
+
 
 @app.get("/sqlalchemy")
 def test_posts(db: Session = Depends(get_db)):
-    return {"status": "Success"}
+    
+    posts = db.query(models.Post).all() # Query all posts from the database
+    
+    return {"data": posts}
+
 
 @app.get("/posts")
 def get_posts():
@@ -69,20 +72,20 @@ def get_posts():
 
     return {"data": posts}
 
-# Extract data and send it back
+
 @app.post("/posts", status_code=status.HTTP_201_CREATED) 
-def create_posts(post: Post): # Automaticly extracts the data via post
+def create_posts(post: Post): 
     cursor.execute("""INSERT INTO posts (title, content, published) VALUES (%s, %s, %s) RETURNING * """, 
                    (post.title, post.content, post.published))
-    # %s is a variable that is used to insert data into the database
-    # First %s is the title, second %s is the content, third %s is the published
-    
+
     new_post = cursor.fetchone() # Fetches the returned data from the database
     conn.commit() # Commits the data to the database
+    
     return {"data": new_post} # Returns the post
 
+
 @app.get("/posts/{id}") # Extracts the id from the url, the id is a path parameter
-def get_post(id: int): #Fast api will auto extract that id and we can pass it directly to function
+def get_post(id: int):
     cursor.execute("""SELECT * FROM posts WHERE id = %s""", (id,))
     post = cursor.fetchone()    
  
@@ -91,6 +94,7 @@ def get_post(id: int): #Fast api will auto extract that id and we can pass it di
                             detail= f"Post with id {id} not found") # Raises an error if post is not found
     print(post)
     return {"post_detail": post}
+
 
 @app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)  # Extracts the id from the url, the id is a path parameter
 def delete_post(id: int):
@@ -108,7 +112,8 @@ def delete_post(id: int):
 @app.put("/posts/{id}")
 def update_post(id: int, post: Post):
     
-    cursor.execute("""Update posts SET title = %s, content = %s, published = %s WHERE id = %s RETURNING * """, (post.title, post.content, post.published, id))
+    cursor.execute("""Update posts SET title = %s, content = %s, published = %s WHERE id = %s RETURNING * """, 
+                   (post.title, post.content, post.published, id))
     updated_post = cursor.fetchone()
     conn.commit()
     
