46fc2ced8777303f2b8a50797e1ceda05240c80c
Dena Azarang <dena.azarang@hotmail.com>
2022-07-15 22:31:16 +0200

    Added queries

--- app/routers/post.py
+++ app/routers/post.py
@@ -1,5 +1,5 @@
 from fastapi import Body, FastAPI, Response, status, HTTPException, Depends, APIRouter
-from typing import List
+from typing import List, Optional
 from sqlalchemy.orm import Session
 from .. import models, schemas, oauth2
 from ..database import get_db
@@ -8,11 +8,13 @@ router = APIRouter(prefix="/posts", tags=["Posts"])
 
 @router.get("/", response_model = List[schemas.Post])
 def get_posts(db: Session = Depends(get_db),
-              current_user: int = Depends(oauth2.get_current_user)):
+              current_user: int = Depends(oauth2.get_current_user),
+              limit: int = 10, skip: int = 0, search: Optional[str] = ""):
     # cursor.execute("""SELECT * FROM posts """)
     # posts = cursor.fetchall()
     
-    posts = db.query(models.Post).all()
+    print(search)
+    posts = db.query(models.Post).filter(models.Post.title.contains(search)).limit(limit).offset(skip).all()
     # posts = db.query(models.Post).filter(models.Post.owner_id == current_user.id).all() # Only show posts that belong to the current user
 
     return posts
@@ -47,10 +49,11 @@ def get_post(id: int,  db: Session = Depends(get_db),
     if not post: 
         raise HTTPException(status_code= status.HTTP_404_NOT_FOUND, 
                             detail= f"Post with id {id} not found") # Raises an error if post is not found
-        
-    if post.owner_id != current_user.id:
-        raise HTTPException(status_code= status.HTTP_403_FORBIDDEN,
-                            detail=f"Not authorized to perform request action")
+    
+    # To check if the post belongs to the current user:
+    # if post.owner_id != current_user.id:
+    #     raise HTTPException(status_code= status.HTTP_403_FORBIDDEN,
+    #                         detail=f"Not authorized to perform request action")
     
     return post
 
