487e8418d114e1c880fe0cd0c4639ea186afa6ff
Dena Azarang <dena.azarang@hotmail.com>
2022-07-15 19:47:32 +0200

    Added get_current_user function

--- app/oauth2.py
+++ app/oauth2.py
@@ -1,6 +1,11 @@
 from jose import JWTError, jwt
 from datetime import datetime, timedelta
 from . import schemas
+from fastapi import Depends, status, HTTPException
+from fastapi.security import OAuth2PasswordBearer
+
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
 
 # SECRET_KEY
 #Algorithm
@@ -36,4 +41,7 @@ def verify_access_token(token: str, credentials_exception):
     except JWTError:
         raise credentials_exception
     
-    
+
+def get_current_user(token: str = Depends(oauth2_scheme)):
+    credentials_exception = HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, 
+                                          detail="Could not validate credentials")
