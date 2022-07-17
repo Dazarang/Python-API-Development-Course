d0f6a4c898c51c2a8f88172041f8e8c3e520a22e
Dena Azarang <dena.azarang@hotmail.com>
2022-07-17 19:58:59 +0200

    Added post verification before vote

--- app/routers/vote.py
+++ app/routers/vote.py
@@ -8,6 +8,11 @@ router = APIRouter(prefix="/vote", tags=["Vote"])
 @router.post("/", status_code=status.HTTP_201_CREATED)
 def vote(vote: schemas.Vote, db: Session = Depends(database.get_db), current_user: int = Depends(oauth2.get_current_user)):
     
+    post = db.query(models.Post).filter(models.Post.id == vote.post_id).first()
+    if not post:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f'Post with id: {vote.post_id} does not exist')
+    
     vote_query = db.query(models.Vote).filter(models.Vote.post_id == vote.post_id, models.Vote.user_id == current_user.id)
     found_vote = vote_query.first()
     
@@ -15,7 +20,6 @@ def vote(vote: schemas.Vote, db: Session = Depends(database.get_db), current_use
         if found_vote:
             raise HTTPException(status_code= status.HTTP_409_CONFLICT,
                                 detail=f'User {current_user.id} has already voted for post {vote.post_id}')
-            
         new_vote = models.Vote(post_id = vote.post_id, user_id = current_user.id)
         db.add(new_vote)
         db.commit()
@@ -25,8 +29,7 @@ def vote(vote: schemas.Vote, db: Session = Depends(database.get_db), current_use
     else: 
         if not found_vote:
             raise HTTPException(status_code= status.HTTP_404_NOT_FOUND,
-                                detail=f'Vote does not exist')
-            
+                                detail=f'Vote does not exist')    
         vote_query.delete(synchronize_session=False)
         db.commit()
         
